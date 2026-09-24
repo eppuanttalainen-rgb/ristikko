c8c6684a5d329ffa509041d9ad89f1cac0bea2ab
eppuanttalainen-rgb <eppuanttalainen@gmail.com>
2026-09-24 22:47:27 +0300

    Add soft-launch service worker

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE='sanaristeys-rc5-softlaunch-v1';
+const CORE=['./','./index.html','./privacy.html','./updates.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k.startsWith('sanaristeys-')&&k!==CACHE).map(k=>caches.delete(k))
+  )).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(
+    fetch(event.request).then(response=>{
+      if(response&&response.ok){
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      }
+      return response;
+    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+  );
+});
