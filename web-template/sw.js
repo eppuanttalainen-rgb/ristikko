cd5951c8903e6f724cd2e266506621a888249d98
eppuanttalainen-rgb <eppuanttalainen@gmail.com>
2026-09-24 20:46:25 +0300

    Add offline service worker template

---
+++ web-template/sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'mummon-ristikko-rc1-v1';
+const CORE = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
